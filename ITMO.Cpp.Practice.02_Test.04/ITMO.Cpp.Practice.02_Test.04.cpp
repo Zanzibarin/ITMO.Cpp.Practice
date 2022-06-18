780d1238c9ce8cd122ffe0ee4f703266ddbed217
Александр <alex_potemkin@mail.ru>
2022-06-18 22:54:58 +0300

    Update

---
+++ ITMO.Cpp.Practice.02_Test.04/ITMO.Cpp.Practice.02_Test.04.cpp
@@ -0,0 +1,55 @@
+﻿// ITMO.Cpp.Practice.02_Test.04.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Контрольные задания.\n";
+    cout << "Задание 4. Стрельба по мишени\n";
+
+    int x, y;
+    int k = 0;
+    int a = 10, b = 5;
+
+
+
+    do 
+    {
+        cout << "Сместите прицел по оси X: ";
+        cin >> x;
+        cout << "Сместите прицел по оси Y: ";
+        cin >> y;
+
+        if (x * x + y * y < 9 && y > 0) 
+        {
+            k = k + a;
+        }
+        else if (x * x + y * y > 9 || y < 0) 
+        {
+            k = k + b;
+        }
+
+        cout << "Очки, набранные по итогу выстрела: " << k << endl;
+
+    } while (k < 20);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
