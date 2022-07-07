841279f85584827240189cb814c09fc7e4da2758
Александр <alex_potemkin@mail.ru>
2022-07-07 19:02:39 +0300

    Update

---
+++ ITMO.Cpp.Practice.04_Task.01_Ex.01_ParamTransit/ITMO.Cpp.Practice.04_Task.01_Ex.01_ParamTransit.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+void fum_value(double k, double x, double y)
+{
+    x = x + k;
+    y = y + k;
+}
+
+void fum_ptr(double k, double* x, double* y)
+{
+    *x = *x + k;
+    *y = *y + k;
+}
+
+void fum_ref(double k, double& x, double& y)
+{
+    x = x + k;
+    y = y + k;
+}
+
+void print(double x, double y)
+{
+    cout << "x = " << x << "; y = " << y << endl;
+}
+
+
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Практическое занятие 4. Использование указателей и ссылок" << endl;
+    cout << "Упражнение 1. Передача параметров" << endl << endl;
+
+    double k = 2.5;
+    double xv = 10;
+    double yv = 10;
+
+    print(xv, yv);
+
+    fum_value(k, xv, yv); // Передача в функцию обычного параметра 
+    print(xv, yv);
+
+    fum_ptr(k, &xv, &yv); // Передача в функцию параметра указателя 
+    print(xv, yv);
+
+    fum_ref(k, xv, yv); // Передача в функцию параметра ссылки 
+    print(xv, yv);
+
+    return 0;
+}
---
+++ ITMO.Cpp.Practice.04_Task.02_Exchange/ITMO.Cpp.Practice.04_Task.02_Exchange.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+void swap(int*, int*);
+void swap(int&, int&);
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Практическое занятие 4. Использование указателей и ссылок" << endl;
+    cout << "Упражнение 2. Реализация функции обмена значений" << endl << endl;
+
+    int x = 5, y = 10;
+
+    swap(&x, &y);
+    cout << x << " " << y << endl;
+
+    swap(x, y);
+    cout << x << " " << y << endl;
+}
+
+void swap(int* x, int* y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void swap(int& x, int& y)
+{
+    int temp;
+    temp = x;
+    x = y;
+    y = temp;
+}
