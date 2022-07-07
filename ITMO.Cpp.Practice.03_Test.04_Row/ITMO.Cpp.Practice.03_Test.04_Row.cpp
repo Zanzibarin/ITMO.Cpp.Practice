8bb6154a729595b723d37cde714b1450e8884cae
Александр <alex_potemkin@mail.ru>
2022-07-07 19:20:13 +0300

    Update

---
+++ ITMO.Cpp.Practice.03_Test.04_Row/ITMO.Cpp.Practice.03_Test.04_Row.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int sumFives(int a);
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Контрольные задания" << endl;
+    cout << "Задание 4. Рекурсивная функция суммы ряда" << endl << endl;
+
+
+    int a;
+    std::cout << "Укажите число для ряда из сумм числа 5\n";
+    std::cin >> a;
+    std::cout << sumFives(a);
+}
+
+int sumFives(int a)
+{
+    if (a == 1) return 5;
+    else return(a * 5 + sumFives(a - 1));
+}
---
+++ ITMO.Cpp.Practice.03_Test.05_Binary/ITMO.Cpp.Practice.03_Test.05_Binary.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int converter(int a);
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Контрольные задания" << endl;
+    cout << "Задание 5. Применение рекурсии для перевода целого числа в двоичный код" << endl << endl;
+
+    int a;
+    std::cout << "Введите число для конвертации из десятичной системы в двоичную\n";
+    std::cin >> a;
+    converter(a);
+
+}
+
+int converter(int a)
+{
+    int remainder(0);
+    if (a == 2)
+    {
+        std::cout << a;
+        return a;
+    }
+    else
+    {
+        converter(a / 2);
+        std::cout << a % 2;
+        return a;
+    }
+}
